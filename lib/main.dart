f3d1600fd2f148f511176a3b9f1295345a0f37c4
LisisFd <lisis.fd@gmail.com>
2023-10-12 17:49:18 +0300

    fix env bug

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,6 @@ import 'package:app_main/app_main.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
-  await dotenv.load();
+  await dotenv.load(fileName: 'assets/.env');
   runApp(const MyApp());
 }
