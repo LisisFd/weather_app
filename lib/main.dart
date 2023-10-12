ea8b40c1478864a951b9976e6dd72bb12ad28527
LisisFd <lisis.fd@gmail.com>
2023-10-12 18:21:19 +0300

    update .env -> dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,6 @@ import 'package:app_main/app_main.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
-  await dotenv.load(fileName: 'assets/.env');
+  await dotenv.load(fileName: 'assets/dotenv');
   runApp(const MyApp());
 }
